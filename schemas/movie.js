c12ee4643908e853e8004fdd8fdf6bb7d5abef87
dingshaohua <345561171@qq.com>
2016-07-05 00:10:31 +0800

    mongo schema module

---
+++ schemas/movie.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose')
+
+var MovieSchema = new mongoose.Schema({
+	doctor: String,
+	performer: String,
+	title: String,
+	language: String,
+	country: String,
+	summary: String,
+	flash: String,
+	poster: String,
+	year: Number,
+	meta: {
+		createAt: {
+			type: Date,
+			default: Date.now()
+		},
+		updataAt: {
+			type: Date,
+			default: Date.now()
+		}
+	}
+})
+
+MovieSchema.pre('save', function(next) {
+	if (this.isNew) {
+		this.meta.createAt = this.meta.updataAt = Date.now()
+	} else {
+		this.meta.updataAt = Date.now()
+	}
+
+	next()
+})
+
+MovieSchema.statics = {
+	fetch: function(cb) {
+		return this.find({}).sort('meta.updataAt').exec(cb)
+	},
+	findById: function(id, cb) {
+		return this.findOne({_id: id}).exec(cb)
+	}
+}
+
+module.exports = MovieSchema
